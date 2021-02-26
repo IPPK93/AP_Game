0cd849155d58c84f19f0bcd47cc6ef0acc4c1c04
IPPK93 <karimov.pd@phystech.edu>
2021-02-26 23:05:58 +0300

    Add camera scroll

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ game_map = [ ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '
 TILE_SIZE = 50
 STEP = 2
 
-WIN_SIZE_X, WIN_SIZE_Y = TILE_SIZE * len(game_map[0]), TILE_SIZE * len(game_map) # TILE_SIZE * 12, TILE_SIZE * 11
+WIN_SIZE_X, WIN_SIZE_Y = TILE_SIZE * 12, TILE_SIZE * 8 # TILE_SIZE * len(game_map[0]), TILE_SIZE * len(game_map)
 
 
 
@@ -60,58 +60,85 @@ back.fill((200, 200, 200))
 wall_rect = wall.get_rect()
 back_rect = back.get_rect()
 
+
+camera = [0, 0]
+offset_x, offset_y = 50, 50
+
 player_surf = pygame.image.load('player.png').convert()
 player_surf.set_colorkey((255, 255, 255))
 player_surf = pygame.transform.scale(player_surf, (TILE_SIZE//2, TILE_SIZE//2))
-player_rect = pygame.Rect(50, 100, player_surf.get_width(), player_surf.get_height())
+player_rect = pygame.Rect(max(TILE_SIZE, camera[0] + offset_x), max(TILE_SIZE, camera[1] + offset_y), player_surf.get_width(), player_surf.get_height())
 
 
 player_velocity = [0, 0]
 
-# scroll = [0, 0]
+move = {'right' : False, 'left' : False, 'up' : False}
+
+camera_x, camera_y = 0, 0
 
 while running:
     
     screen.fill((200, 200, 200))
     
     tiles = []
-    
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-            if event.key == pygame.K_RIGHT:
-                player_velocity[0] += 3
-            if event.key == pygame.K_LEFT:
-                player_velocity[0] -= 3
-            if event.key == pygame.K_UP:
-                player_velocity[1] -= 5
     
+    pressed = pygame.key.get_pressed()
+    
+    if pressed[pygame.K_UP]:
+        player_velocity[1] -= 1
+    if pressed[pygame.K_RIGHT]:
+        player_velocity[0] += 1
+    if pressed[pygame.K_LEFT]:
+        player_velocity[0] -= 1
+                
     for i in range(len(game_map)):
         for j in range(len(game_map[0])):
-            x_coord, y_coord = j * TILE_SIZE, i * TILE_SIZE
+            x_coord, y_coord = j * TILE_SIZE - camera[0], i * TILE_SIZE - camera[1]
             if game_map[i][j] == '1':
                 screen.blit(wall, (x_coord, y_coord))
                 tiles.append(pygame.Rect(x_coord, y_coord, TILE_SIZE, TILE_SIZE))
             elif game_map[i][j] == '0':
                 screen.blit(back, (x_coord, y_coord))
-    
-#     scroll[0] -= player_rect.x
-#     scroll[1] -= player_rect.y
-    
+               
+            
+            
     collision = move_player(player_rect, list(map(round, player_velocity)), tiles)
     
-#     scroll[0] += player_rect.x
-#     scroll[1] += player_rect.y
+    
+#     print(f"WIN_SIZE_X - offset_x: {WIN_SIZE_X - offset_x}")
+#     print(f"player_rect.right: {player_rect.right}")
+#     print(f"offset_x: {offset_x}")
+#     print(f"player_rect.left: {player_rect.left}")
+#     print()
+#     print(f"offset_y: {offset_y}")
+#     print(f"player_rect.top: {player_rect.top}")
+#     print(f"WIN_SIZE_Y - offset_y: {WIN_SIZE_Y - offset_y}")
+#     print(f"player_rect.bottom: {player_rect.bottom}")
+#     print()
+    
+    if WIN_SIZE_X - offset_x < player_rect.right:
+        camera_x = player_rect.right - (WIN_SIZE_X - offset_x)
+    if offset_x > player_rect.left:
+        camera_x = player_rect.left - offset_x
+    if offset_y > player_rect.top:
+        camera_y = player_rect.top - offset_y
+    if WIN_SIZE_Y - offset_y < player_rect.bottom:
+        camera_y = player_rect.bottom - (WIN_SIZE_Y - offset_y)
+
     
     if collision['down']:
         player_velocity[0] *= 0.85
     else:
         player_velocity[0] *= 0.99
 
-    player_velocity[1] += 0.8
+    player_velocity[1] += 0.4
     
     if collision['down']:
         player_velocity[1] = 0
@@ -122,13 +149,17 @@ while running:
     if collision['right']:
         player_velocity[0] = 0
         
+    camera[0] += camera_x
+    player_rect.x -= camera_x
+    camera[1] += camera_y
+    player_rect.y -= camera_y
+    camera_x = 0
+    camera_y = 0
     
-
     screen.blit(player_surf, player_rect)
     pygame.display.flip()
     clock.tick(60)
-    print(f'Velocity: {player_velocity}')
-#     print('scroll: ', scroll)
+#     print(f'Velocity: {player_velocity}')
     
 pygame.display.quit()
 pygame.quit()
