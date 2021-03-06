6748aa4e333a134bc8a8fcc1ddbb5faaaedd191d
IPPK93 <karimov.pd@phystech.edu>
2021-03-06 18:19:12 +0300

    Add enemy + make map loading from file + change tile and window sizes

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-from physics import *
-from camera import *
 from game_objects import *
 from field import *
-
 import pygame
 
 
 pygame.init()
 
-game_map = [ ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'], 
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-             ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'] ]
+TILE_SIZE = 60
 
-
-TILE_SIZE = 50
-
-WIN_SIZE = (TILE_SIZE * 12, TILE_SIZE * 8)
+WIN_SIZE = (800, 600)
 OFFSET = (50, 50)
 
 wall = pygame.Surface((TILE_SIZE, TILE_SIZE))
 wall.fill((200, 0, 0))
 wall_rect = wall.get_rect()
 
-field = Field(pygame.display.set_mode(WIN_SIZE), game_map, {'1' : wall}, TILE_SIZE, Player('player.png', (TILE_SIZE//2, TILE_SIZE//2), 100, 0, 100))
+field = Field(pygame.display.set_mode(WIN_SIZE), {'w' : wall}, TILE_SIZE, Player('player.png', (TILE_SIZE//2, TILE_SIZE//2), 100, 0, 100))
+field.load_map('maps/01.txt')
+
 field.player.set_transparent_color((255, 255, 255))
-field.player.rect = field.player.rect.move(max(TILE_SIZE, field.camera.x + field.camera.offset_w), max(TILE_SIZE, field.camera.y + field.camera.offset_h))
+field.player.rect = field.player.rect.move(field.camera.rect.x + field.camera.offset_w + 2* TILE_SIZE, field.camera.rect.y + field.camera.offset_h + TILE_SIZE)
 
 clock = pygame.time.Clock()
 
+field.enemies.append(Enemy('enemy_1.png', (TILE_SIZE//2, TILE_SIZE//2), 100, 0, 100))
+field.enemies[-1].rect = field.enemies[-1].rect.move(field.camera.rect.x + field.camera.offset_w + TILE_SIZE, field.camera.rect.y + field.camera.offset_h + TILE_SIZE) 
 
 running = True
 while running:
@@ -46,14 +34,10 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-
-    field.build_map()
-
-    field.player.move(field.tiles)
-        
+    
     field.update()
-
-    clock.tick(60)               
+    
+    clock.tick(60)
     
 pygame.display.quit()
 pygame.quit()
