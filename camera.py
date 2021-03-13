46e3a90cb4d49ae2469e0cfc7e01372a5d306b20
IPPK93 <karimov.pd@phystech.edu>
2021-03-13 17:54:08 +0300

    Change update() function argument

--- camera.py
+++ camera.py
@@ -7,15 +7,15 @@ class Camera():
         self.rect = pygame.Rect((0, 0), size)
         self.offset_w, self.offset_h = offset
         
-    def update(self, obj_rect):
+    def update(self, obj):
         dx, dy = 0, 0
-        if self.rect.x + self.rect.width - self.offset_w < obj_rect.right:
-            dx = obj_rect.right - (self.rect.x + self.rect.width - self.offset_w)
-        if self.rect.x + self.offset_w > obj_rect.left:
-            dx = obj_rect.left - (self.rect.x + self.offset_w)
-        if self.rect.y + self.offset_h > obj_rect.top:
-            dy = obj_rect.top - (self.rect.y + self.offset_h)
-        if self.rect.y + self.rect.height - self.offset_h < obj_rect.bottom:
-            dy = obj_rect.bottom - (self.rect.y + self.rect.height - self.offset_h)
+        if self.rect.x + self.rect.width - self.offset_w < obj.rect.right:
+            dx = obj.rect.right - (self.rect.x + self.rect.width - self.offset_w)
+        if self.rect.x + self.offset_w > obj.rect.left:
+            dx = obj.rect.left - (self.rect.x + self.offset_w)
+        if self.rect.y + self.offset_h > obj.rect.top:
+            dy = obj.rect.top - (self.rect.y + self.offset_h)
+        if self.rect.y + self.rect.height - self.offset_h < obj.rect.bottom:
+            dy = obj.rect.bottom - (self.rect.y + self.rect.height - self.offset_h)
         self.rect.x += dx
         self.rect.y += dy
