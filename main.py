31d01fc04868ecb5cabe6b91c79f4b8cfc3b8fd4
IPPK93 <karimov.pd@phystech.edu>
2021-04-03 21:23:10 +0300

    Add second enemy for tests + change imports

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from game_objects import *
+from live_objects import *
+from lifeless_objects import *
 from field import *
 import pygame
 
 
 pygame.init()
+clock = pygame.time.Clock()
 
 TILE_SIZE = 48
 
 WIN_SIZE = (800, 600)
 OFFSET = (50, 50)
-
-# wall = pygame.Surface((TILE_SIZE, TILE_SIZE))
-# wall.fill((200, 0, 0))
-# wall_rect = wall.get_rect()
 
 field = Field(pygame.display.set_mode(WIN_SIZE), {'w' : StaticBlock((TILE_SIZE, TILE_SIZE))}, TILE_SIZE, Player('player.png', (TILE_SIZE//2, TILE_SIZE//2), 100, 0, 100))
 field.load_map('maps/01.txt')
@@ -20,10 +18,12 @@ field.load_map('maps/01.txt')
 field.player.set_transparent_color((255, 255, 255))
 field.player.rect = field.player.rect.move(field.camera.rect.x + field.camera.offset_w + 2* TILE_SIZE, field.camera.rect.y + field.camera.offset_h + TILE_SIZE)
 
-clock = pygame.time.Clock()
+field.enemies.append(MovingGuy('enemy_1.png', (TILE_SIZE, TILE_SIZE), 100, 0, 100, target = field.player))
+field.enemies[-1].rect = field.enemies[-1].rect.move(field.camera.rect.x + field.camera.offset_w + 5*TILE_SIZE, field.camera.rect.y + field.camera.offset_h + 3*TILE_SIZE)
 
 field.enemies.append(Enemy('enemy_1.png', (TILE_SIZE//2, TILE_SIZE//2), 100, 0, 100))
 field.enemies[-1].rect = field.enemies[-1].rect.move(field.camera.rect.x + field.camera.offset_w + TILE_SIZE, field.camera.rect.y + field.camera.offset_h + TILE_SIZE) 
+
 
 running = True
 while running:
@@ -38,6 +38,7 @@ while running:
     field.update()
     
     clock.tick(60)
+    
     
 pygame.display.quit()
 pygame.quit()
