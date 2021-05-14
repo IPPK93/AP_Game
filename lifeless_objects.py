397312fe607b67877abb5e69d4f0eeb1cb8b56ba
IPPK93 <karimov.pd@phystech.edu>
2021-05-14 05:41:05 +0300

    Add DynamicBlock class

--- lifeless_objects.py
+++ lifeless_objects.py
@@ -1,24 +1,34 @@
 from game_object import GameObject
+from copy import copy
 
 class LifelessObject(GameObject):
     def __init__(self, image, size, destructibility):
         super().__init__(image, size)
         self.destructible = destructibility
-        
+
 class Block(LifelessObject):
     def __init__(self, image, size, destructibility):
         super().__init__(image, size, destructibility)
         
+    def get_moved_block(self, *position):
+        new_block = copy(self)
+        new_block.rect = new_block.rect.move(*position)
+        return new_block
 
 class StaticBlock(Block):
     def __init__(self, size, image = 'temp_block.png'):
         super().__init__(image, size, False)
         
-    def get_moved_block(self, *position):
-        new_block = StaticBlock((self.width, self.height), self.image)
-        new_block.rect = new_block.rect.move(*position)
-        return new_block
+class DynamicBlock(Block):
+    def __init__(self, size, destructibility, move_limit = 100, image = 'temp_block_2.png'):
+        super().__init__(size, destructibility, image)
+        self.move_counter = 0
+        self.move_limit = move_limit
+        self.move_right = True
         
-class GameItem(LifelessObject):
-    def __init__(self, image, size):
-        super().__init__(image, size, False)  
+    def move(self, *group):
+        self.rect.x += self.move_right - (not self.move_right)
+        self.move_counter += 1
+        self.move_counter %= self.move_limit
+        if self.move_counter == 0:
+            self.move_right = not self.move_right
